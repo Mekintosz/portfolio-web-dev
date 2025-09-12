ade69247aa450f058436069006acf77e22f5555a
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-09-12 12:58:57 +0200

    refactor navOperation.js and fix croos/hamburger issue in mobile wiev

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,34 @@ const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.querySelector(".nav__list");
 const hamburger = document.getElementById("hamburger");
 const cross = document.getElementById("cross");
+const sections = document.querySelectorAll("section[id]");
 
 export const navOpperations = () => {
-  menuToggle.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-    hamburger.classList.toggle("active");
-    cross.classList.toggle("active");
-  });
+  const closeOpenNavbar = () => {
+    if (window.innerWidth < 768) {
+      const isActive = navbar.classList.toggle("active");
 
-  const activePage = () => {
+      hamburger.classList.toggle("active", !isActive);
+      cross.classList.toggle("active", isActive);
+    }
+  };
+
+  const deactivateLinks = () => {
     navLinks.forEach((link) => {
       link.classList.remove("active");
     });
 
-    navbar.classList.remove("active");
+    closeOpenNavbar();
   };
+
+  menuToggle.addEventListener("click", () => {
+    closeOpenNavbar();
+  });
 
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
       if (!link.classList.contains("active")) {
-        activePage();
+        deactivateLinks();
         link.classList.add("active");
       }
     });
