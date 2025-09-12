ae8a993d1b0db9702f923c7b48ec57859b66e356
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-09-12 16:03:29 +0200

    Updale download link to meta.env.BASE_URL for GH pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { navOperations } from "./js/main.js";
 import { toggleSetup } from "./js/theme-togle.js";
+import { downloadCV } from "./js/downloadCV.js";
 import "./scss/main.scss";
 
 navOperations();
 toggleSetup();
+downloadCV();
---
+++ src/js/downloadCV.js
@@ -0,0 +1,12 @@
+export const downloadCV = () => {
+  // Base path for GitHub Pages
+  const base = import.meta.env.BASE_URL; // automatically handles subpath
+
+  // PDF file path
+  const pdfPath = `${base}Michał Łazicki CV.pdf`;
+
+  // Open PDF in a new tab
+  document.getElementById("downloadBtn").addEventListener("click", () => {
+    window.open(pdfPath, "_blank");
+  });
+};
