56a2f75dbe63fd011f49b29ea671c80eec31136f
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-09-22 16:08:21 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // publishing to https://Mekintosz.github.io/portfolio-web-dev/
-  base: "/portfolio-web-dev/",
+  base: "./",
 });
