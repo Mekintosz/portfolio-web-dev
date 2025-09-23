49a0d07eda252c76a6a01fbd895b8c04e45e5e08
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-09-24 01:49:49 +0200

    Add favicon and meta tags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { navOperations } from "./js/main.js";
-import { toggleSetup } from "./js/theme-togle.js";
+import { toggleSetup } from "./js/theme-toggle.js";
 import { downloadCV } from "./js/downloadCV.js";
 import "./scss/main.scss";
 
